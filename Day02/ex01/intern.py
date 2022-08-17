ee25e7e4127e7cf69d28131b03e219755caec8a8
Yasin Bestrioui <72823168+Toyama70@users.noreply.github.com>
2022-08-17 11:09:39 +0200

    Add files via upload

---
+++ Day02/ex01/intern.py
@@ -0,0 +1,30 @@
+class Intern:
+	def __init__(self, name="nobody"):
+		self.name = name
+		if (name=="nobody"):
+			print("My name ? I'm " + name + ", an intern, I have no name")
+		else :
+			print("My name is " + name + ", nice to meet you !")
+	
+	class Coffee:
+		def __str__():
+			print("This is the worst coffee you ever tasted.")
+	
+	def __del__(self):
+		print(self.name + " died from working too much")
+
+	def __str__(self):
+		return self.name
+
+	def make_coffee():
+		latte = Intern.Coffee()
+		return latte
+	
+	def work():
+		pass
+
+if __name__ == '__main__':
+	hello2 = Intern()
+	hello = Intern("Mark")
+	hello.Coffee.__str__()
+	del hello
